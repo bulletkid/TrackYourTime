81ed8b135f1ff38b264d6213b6be61352aeefdd4
Manu Anand <mananan@microsoft.com>
2020-10-18 23:49:33 +0530

    Python 3 changes

--- rescuetime.py
+++ rescuetime.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-
+##from __future__ import print_function
 """Find the currently active window."""
 
 import logging
@@ -11,13 +10,13 @@ import sys
 import signal
 
 def signal_handler(signal, frame): 
-	print('You pressed Ctrl+C!') 
+    print('You pressed Ctrl+C!') 
 
-	## Do the calculations here
+    ## Do the calculations here
 
-	print ('** INCOMPLETE SESSION(S) !! ** ')
+    print ('** INCOMPLETE SESSION(S) !! ** ')
 
-	sys.exit(0) 
+    sys.exit(0) 
 
 
 
@@ -26,64 +25,70 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     stream=sys.stdout)
 
 def main (argv):
-	try:
-		opts, args = getopt.getopt(argv, "hs:c:n:",["session=","category=", "number"]) 
-	except getopt.GetoptError: 
-		print './rescuetime.py -s <sessionName> -c <category> -n <numSessions>' 
-		sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv, "hs:c:n:",["session=","category=", "number"]) 
+    except getopt.GetoptError: 
+        print('./rescuetime.py -s <sessionName> -c <category> -n <numSessions>') 
+        sys.exit(2)
 
-	signal.signal(signal.SIGINT, signal_handler) 
-	sessionName = ''
-	category = 'Work'
-	numSessions = 1
-	for opt, arg in opts:
-		if opt == "-h":
-			print './rescuetime.py -s <sessionName> -c <category> -n <number>' 
-			sys.exit(2)
-		elif opt in ("-s", "--session" ):
-			sessionName = arg
-		elif opt in ("-c", "--category" ):
-			category = arg
-		elif opt in ("-n", "--number" ):
-			numSessions = arg
+    print(f"Name of the script      : {sys.argv[0]=}")
+    print(f"Arguments of the script : {sys.argv[1:]=}")
 
-	
-	UTF8Writer = codecs.getwriter('utf8')
-	sys.stdout = UTF8Writer(sys.stdout)
-	
-	timeSlept = 0
-	timeToSleep = 30
+    signal.signal(signal.SIGINT, signal_handler) 
+    sessionName = "Default"
+    category = "Work"
+    numSessions = 1
+    for opt, arg in opts:
+        if opt == "-h":
+            print('./rescuetime.py -s <sessionName> -c <category> -n <number>') 
+            sys.exit(2)
+        elif opt in ("-s", "--session" ):
+            sessionName = arg
+        elif opt in ("-c", "--category" ):
+            category = arg
+        elif opt in ("-n", "--number" ):
+            numSessions = arg
 
-	print ( sessionName, category, numSessions )
-	while True:
+    ##UTF8Writer = codecs.getwriter('utf8')
+    ##sys.stdout = UTF8Writer(sys.stdout)
+    
+    timeSlept = 0
+    timeToSleep = 30
 
-	    localtime = time.asctime( time.localtime(time.time()) )
-	    #print("Active window: %s" % str(localtime get_active_window()))
-	    #print("Active window: %s" % str(localtime get_active_window()))
-	    #print( localtime,str(get_active_window()) )
-	    #activeWindow = get_active_window()
-	    #activeWindow =  u' '.activeWindow.encode('utf-8').strip()
-	    activeWindow = unicode(get_active_window())
+    demoString = "Demo"
+    print ((demoString))
+    print( f"{demoString}" )
+    print (demoString)
+    print( sessionName,category,numSessions )
+    while True:
 
-
-	    ## TODO: Add logic for maintaining a session specific count of activities
-	    ## This follows on the other broader goal of classifying activities as +ve or -ve (or have a weight attached to them)
-	    ## Will aid in creating a focus score
-
-
-	    ## TODO: This app should also have a taskbar UI equivalent that allows us to easily add session
+            localtime = time.asctime( time.localtime(time.time()) )
+            #print("Active window: %s" % str(localtime get_active_window()))
+            #print("Active window: %s" % str(localtime get_active_window()))
+            #print( localtime,str(get_active_window()) )
+            #activeWindow = get_active_window()
+            #activeWindow =  u' '.activeWindow.encode('utf-8').strip()
+            activeWindow = str(get_active_window())
 
 
-	    #print( localtime, get_active_window() )
-	    print( localtime, activeWindow )
-	    time.sleep(timeToSleep)
+            ## TODO: Add logic for maintaining a session specific count of activities
+            ## This follows on the other broader goal of classifying activities as +ve or -ve (or have a weight attached to them)
+            ## Will aid in creating a focus score
 
-	    timeSlept += timeToSleep
-	    if timeSlept > 25*60*numSessions :
-		    break
 
-	print ('** SESSION(S) COMPLETE!! Many Congrats!!** ')
-	    	
+            ## TODO: This app should also have a taskbar UI equivalent that allows us to easily add session
+
+
+            #print( localtime, get_active_window() )
+            print(( localtime, activeWindow ))
+            time.sleep(timeToSleep)
+
+            timeSlept += timeToSleep
+            if timeSlept > 25*60*numSessions :
+                break
+
+    print ('** SESSION(S) COMPLETE!! Many Congrats!!** ')
+                
 def get_active_window():
     """
     Get the currently active window.
@@ -134,60 +139,61 @@ def get_active_window():
         # http://stackoverflow.com/a/373310/562769
         from AppKit import NSWorkspace
         from Quartz import (
-        	CGWindowListCopyWindowInfo,
-        	kCGWindowListOptionOnScreenOnly,
-	        kCGNullWindowID
-    	)
+                    CGWindowListCopyWindowInfo,
+                    kCGWindowListOptionOnScreenOnly,
+                    kCGNullWindowID
+            )
 
         active_window_name = (NSWorkspace.sharedWorkspace()
                               .activeApplication()['NSApplicationName'])
-	try:
-		if sys.platform == "darwin":
-		    app = NSWorkspace.sharedWorkspace().frontmostApplication()
-		    active_app_name = app.localizedName()
-		    #print("Active App(NEW) %s" % str(active_app_name) )
-		    #print("Active App(OLD) %s" % str(active_window_name) )
+        try:
+            if sys.platform == "darwin":
+                    app = NSWorkspace.sharedWorkspace().frontmostApplication()
+                    active_app_name = app.localizedName()
+                    #print("Active App(NEW) %s" % str(active_app_name) )
+                    #print("Active App(OLD) %s" % str(active_window_name) )
 
-		    options = kCGWindowListOptionOnScreenOnly
-		    windowList = CGWindowListCopyWindowInfo(options, kCGNullWindowID)
-		    windowTitle = 'Unknown'
-		    for window in windowList:
-			windowNumber = window['kCGWindowNumber']
-			ownerName = window['kCGWindowOwnerName']
-			# geometry = window['kCGWindowBounds']
-			windowTitle = window.get('kCGWindowName', u'Unknown')
-			#print("window:title %s" % str(windowTitle) )
-		
-			##if windowTitle and ( ownerName == active_app_name):
-			if windowTitle and ( ownerName == active_window_name):
-			    # logging.debug(
-			    #     'ownerName=%s, windowName=%s, x=%s, y=%s, '
-			    #     'width=%s, height=%s'
-			    #     % (window['kCGWindowOwnerName'],
-			    #        window.get('kCGWindowName', u'Unknown'),
-			    #        geometry['X'],
-			    #        geometry['Y'],
-			    #        geometry['Width'],
-			    #        geometry['Height']))
-	        	    #print("app %s" % str(active_window_name ) )
-	        	    #print(windowTitle )
-                    	    break
+                    options = kCGWindowListOptionOnScreenOnly
+                    windowList = CGWindowListCopyWindowInfo(options, kCGNullWindowID)
+                    windowTitle = 'Unknown'
+                    for window in windowList:
 
-		return active_window_name + " : " + windowTitle
+                      windowNumber = window['kCGWindowNumber']
+                      ownerName = window['kCGWindowOwnerName']
+                      # geometry = window['kCGWindowBounds']
+                      windowTitle = window.get('kCGWindowName', 'Unknown')
+                      #print("window:title %s" % str(windowTitle) )
+                  
+                      ##if windowTitle and ( ownerName == active_app_name):
+                      if windowTitle and ( ownerName == active_window_name):
+                              # logging.debug(
+                              #     'ownerName=%s, windowName=%s, x=%s, y=%s, '
+                              #     'width=%s, height=%s'
+                              #     % (window['kCGWindowOwnerName'],
+                              #        window.get('kCGWindowName', u'Unknown'),
+                              #        geometry['X'],
+                              #        geometry['Y'],
+                              #        geometry['Width'],
+                              #        geometry['Height']))
+                                          #print("app %s" % str(active_window_name ) )
+                                          #print(windowTitle )
+                        break
 
-##                return _review_active_info(active_app_name, windowTitle)
-			
+            return active_window_name + " : " + windowTitle
 
-	except:
-		logging.error('Unexpected error: %s' % sys.exc_info()[0])
-		logging.error('error line number: %s' % sys.exc_traceback.tb_lineno)
-	        return 'Unknown', 'Unknown'
+    ##                return _review_active_info(active_app_name, windowTitle)
+                
+
+        except:
+            logging.error('Unexpected error: %s' % sys.exc_info()[0])
+            logging.error('error line number: %s' % sys.exc_traceback.tb_lineno)
+            return 'Unknown', 'Unknown'
 
     else:
-        print("sys.platform={platform} is unknown. Please report."
-              .format(platform=sys.platform))
-        print(sys.version)
+        print(("sys.platform={platform} is unknown. Please report."
+              .format(platform=sys.platform)))
+        print((sys.version))
     return active_window_name
 
 if __name__ == "__main__": 
-	main(sys.argv[1:])
+    main(sys.argv[1:])
