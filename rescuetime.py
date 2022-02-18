9740912e02cce04cc773ff097e1ce9ed40c48188
Manu Anand <mananan@microsoft.com>
2022-02-18 17:34:22 +0530

    Added category and activity tracing

--- rescuetime.py
+++ rescuetime.py
@@ -8,7 +8,26 @@ import getopt
 import codecs
 import sys
 import signal
+import win32gui
+import win32process
+import win32pdhutil
+import wmi
+import re
 
+## TODO: Show a menu to user if he has spend 15% time on uncategorized activities/categories
+## TODO: Activity calculation should be here - We should be able to run it on demand (NOTE: There is value in both. So keep it here as well as have it separately)
+## TODO: Move category and activities to file and load the following 3 arrays from there
+
+category = {"ConEmu": 2, "FireFox": 1, "Outlook": 0 , "Atom": 2, "vim" : 2, "teams" : 1};
+
+## Specific Activities
+firefoxActivities = { "Youtube" : -2, "elixir" : 2, "stackoverflow" : 2, "cricinfo" : -2, "primevideo" : -2, "netflix" : -2 }; 
+
+## Not every category need activity specific detail
+activityDictionary = {"FireFox": firefoxActivities};
+
+## If you need more granular control of activities, control on per activity basis.
+	
 def signal_handler(signal, frame): 
     print('You pressed Ctrl+C!') 
 
@@ -53,12 +72,10 @@ def main (argv):
     ##sys.stdout = UTF8Writer(sys.stdout)
     
     timeSlept = 0
-    timeToSleep = 30
+    timeToSleep = 1
+    #timeToSleep = 30
+    systemProcesses = dict()
 
-    demoString = "Demo"
-    print ((demoString))
-    print( f"{demoString}" )
-    print (demoString)
     print( sessionName,category,numSessions )
     while True:
 
@@ -68,7 +85,7 @@ def main (argv):
             #print( localtime,str(get_active_window()) )
             #activeWindow = get_active_window()
             #activeWindow =  u' '.activeWindow.encode('utf-8').strip()
-            activeWindow = str(get_active_window())
+            activeWindow = str(get_active_window(systemProcesses))
 
 
             ## TODO: Add logic for maintaining a session specific count of activities
@@ -84,12 +101,13 @@ def main (argv):
             time.sleep(timeToSleep)
 
             timeSlept += timeToSleep
-            if timeSlept > 25*60*numSessions :
+            #if timeSlept > 25*60*numSessions :
+            if timeSlept > 2*60*numSessions :
                 break
 
     print ('** SESSION(S) COMPLETE!! Many Congrats!!** ')
                 
-def get_active_window():
+def get_active_window(systemProcesses):
     """
     Get the currently active window.
 
@@ -100,6 +118,9 @@ def get_active_window():
     """
     import sys
     active_window_name = None
+    parent = ""
+    windowTitle = None
+
     if sys.platform in ['linux', 'linux2']:
         # Alternatives: http://unix.stackexchange.com/q/38867/4784
         try:
@@ -135,6 +156,38 @@ def get_active_window():
         import win32gui
         window = win32gui.GetForegroundWindow()
         active_window_name = win32gui.GetWindowText(window)
+
+        ## See if we can get the application name too
+        ##try:
+        ##    parent = win32gui.GetParent(window)
+        ##except win32api.error:
+        ##    parent = "Unknown"
+        ##print( "Parent is " + str(parent))
+
+        ## Get parent
+        procs = wmi.WMI().Win32_Process()
+
+        ##pycwnd = win32gui.GetForegroundWindow()
+        tid, pid = win32process.GetWindowThreadProcessId(window)
+
+        ## Check process in hashmap
+        parent = "Unknown"
+        if pid in systemProcesses:
+          print ("Found process in dict - Exec is " + systemProcesses[pid] + "\n")
+          parent = systemProcesses[pid]
+        else:
+          #print("Adding process to dict\n")
+          for proc in procs:
+            if proc.ProcessId == pid:
+              print ('pid' + str(pid) )
+              print ('exec' + proc.ExecutablePath)
+              print ('title'+ win32gui.GetWindowText(window))
+              parent = proc.ExecutablePath
+              systemProcesses[pid] = parent
+
+        print( "Parent is " + str(parent))
+
+
     elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
         # http://stackoverflow.com/a/373310/562769
         from AppKit import NSWorkspace
@@ -179,8 +232,8 @@ def get_active_window():
                                           #print(windowTitle )
                         break
 
-            return active_window_name + " : " + windowTitle
 
+            return active_window_name + " : " + windowTitle
     ##                return _review_active_info(active_app_name, windowTitle)
                 
 
@@ -193,7 +246,35 @@ def get_active_window():
         print(("sys.platform={platform} is unknown. Please report."
               .format(platform=sys.platform)))
         print((sys.version))
-    return active_window_name
+
+    ## Match the category
+    ## TODO: Need to use RE here ; Here is a snippet
+    ## if re.search('mandy', 'Mandy Pande', re.IGNORECASE):
+    value = 0
+    activityCategory = "default"
+    for i in set(category.keys()):
+      print ("next category is " +i);
+      #if (parent.find(i)):
+      if (re.search(i, parent, re.IGNORECASE)):
+        activityCategory = i
+        value = category[i]
+        print ("Category is " +i+ " with value " + str(value) )
+        break;
+
+    ## Do we have active directory associated with this category?
+    if activityCategory in activityDictionary :
+      print ("There are more activities associated with category: " +activityCategory);
+      ## Checking if this title matches any?
+      categoryActivities = activityDictionary[activityCategory]
+      for j in set(categoryActivities.keys()):
+        print ("next activity is " +j);
+        if (re.search(j, active_window_name, re.IGNORECASE)):
+          value = categoryActivities[j]
+          print ("Activity " + j + " of Category " + activityCategory + " has value " + str(value) )
+          break;
+
+    return active_window_name + " : " + parent
+    ##+ ":" + str(parent)
 
 if __name__ == "__main__": 
     main(sys.argv[1:])
